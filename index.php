483ffc7b0e60225d9cef844ab393c9683c676059
ItsLars03 <74597365+ItsLars03@users.noreply.github.com>
2023-03-13 09:40:38 +0100

    Bug fixes

--- index.php
+++ index.php
@@ -47,27 +47,6 @@
         color: black;
     }
 
-    .contact-info {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        margin-top: -15px;
-    }
-
-    .contact-info li {
-        color: black;
-    }
-
-    .fas.fa-phone,
-    .fas.fa-envelope,
-    .fas.fa-map-marker-alt {
-        margin-top: 10px;
-    }
-
-    .contact-info li i {
-        margin-right: 10px;
-    }
-
     footer {
         background-color: #000c;
         color: #fff;
@@ -78,10 +57,6 @@
         height: 50px;
         line-height: 50px;
         font-weight: lighter;
-    }
-
-    .fas.fa-circle {
-        color: transparent;
     }
 
     a {
@@ -156,7 +131,8 @@
 
 <body>
 
-
+    <!-- voeg een laadbalk toe -->
+    <div class="loader"></div>
     <!-- wrap de tekst in een container div met de class "text-container" -->
     <div class="text-container">
         <img src="logo-small.png" alt="IT-Wizzard logo" class="logo">
@@ -171,45 +147,39 @@
         </div>
         <div class="bar-container">
             <div class="bar1">
-                <h4>
-
-                    <?php
-                    $ip_address = $_SERVER['REMOTE_ADDR'];
-                    if (filter_var($ip_address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-                        echo $ip_address;
-                    } else {
-                        echo "Geen IPv4-adres beschikbaar.";
-                    }
-                    ?>
-
+                <h4 class="ipv4"><span id="ipv4"></span></h4>
                 </h4>
             </div>
             <div class="bar1">
-                <h4 class="IPv6">
-                    <?php
-                    error_reporting(E_ERROR | E_PARSE);
-
-                    $ip_address = file_get_contents('https://ipv6-test.com/api/myip.php');
-                    if (filter_var($ip_address, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
-                        echo "Je IPv6-adres is: " . $ip_address;
-                    } else {
-                        echo "Geen IPv6-adres beschikbaar.";
-                    }
-                    ?></h4>
+                <h4 class="IPv6"><span id="ipv6"></span></h4>
             </div>
             <div class="bar1">
-                <h4>
-                    <?php
-                    $url = 'http://ip-api.com/json/';
-                    $data = file_get_contents($url);
-                    $result = json_decode($data, true);
-
-                    echo $result['isp'];
-                    ?><h4>
-
+                <h4 class="provider"><span id="provider"></span></h4>
             </div>
         </div>
     </div>
+    <script>
+    // verberg de laadbalk wanneer de gegevens zijn opgehaald
+    fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("ipv4").textContent = data.ip;
+            const ipv6 = window.IPv6 ? window.IPv6.address : "Niet beschikbaar";
+            document.getElementById("ipv6").textContent = ipv6;
+
+            // verberg de laadbalk wanneer de gegevens zijn opgehaald
+            document.querySelector(".loader").style.display = "none";
+        });
+
+    fetch("https://ipwhois.app/json/")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("provider").textContent = data.isp;
+
+            // verberg de laadbalk wanneer de gegevens zijn opgehaald
+            document.querySelector(".loader").style.display = "none";
+        });
+    </script>
     <footer>
         <div class="footer-bar">
             <strong>Onderdeel van <a href="https://it-wizzard.nl/">IT-Wizzard</a></strong>
@@ -218,4 +188,3 @@
 </body>
 
 </html>
-<!-- Einde van de code -->
